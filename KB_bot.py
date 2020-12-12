ebdb96e43c67a83a3c1cf1d0b7fd0671cefa3a84
Kuptuk <49812392+Kuptuk@users.noreply.github.com>
2020-12-12 20:55:40 +0200

    Update KB_bot.py

--- KB_bot.py
+++ KB_bot.py
@@ -443,11 +443,8 @@ async def server(message):
   response = Image.open(io.BytesIO(response.content))
   idraw = ImageDraw.Draw(response)
   gg = client.get_guild(604636579545219072)
-  a, k = gg.members, 0
-  for i in a:
-    if 'Партнёр [Ур. 1]' in str(i.roles) or 'Партнёр [Ур. 2]' in str(i.roles) or 'Партнёр [Ур. 3]' in str(i.roles):
-      k += 1
-    
+  k = len(gg.get_role(688654966675603491).members) + len(gg.get_role(622501656591990784).members) + len(gg.get_role(622501691107049502).members) + len(gg.get_role(769916590686732319).members)
+  
   idraw.text((365, 115), str(len(gg.emojis)), (255, 255, 255), font = ImageFont.truetype(r'./Gothic.ttf', size = 30))
   idraw.text((299, 167), str(len(gg.roles)), (255, 255, 255), font = ImageFont.truetype(r'./Gothic.ttf', size = 30))
   idraw.text((230, 243), str(gg.verification_level), (255, 255, 255), font = ImageFont.truetype(r'./Gothic.ttf', size = 25))
@@ -456,7 +453,7 @@ async def server(message):
   idraw.text((540, 87), str(len(gg.voice_channels)), (255, 255, 255), font = ImageFont.truetype(r'./Gothic.ttf', size = 30))
   idraw.text((570, 142), str(len(gg.categories)), (255, 255, 255), font = ImageFont.truetype(r'./Gothic.ttf', size = 30))
   idraw.text((619, 215), str(len(gg.text_channels)), (255, 255, 255), font = ImageFont.truetype(r'./Gothic.ttf', size = 30))
-  idraw.text((645, 290), str(gg.premium_subscription_count), (255, 255, 255), font = ImageFont.truetype(r'./Gothic.ttf', size = 30))
+  idraw.text((657, 292), str(gg.premium_subscription_count), (255, 255, 255), font = ImageFont.truetype(r'./Gothic.ttf', size = 30))
   idraw.text((665, 350), str(k), (255, 255, 255), font = ImageFont.truetype(r'./Gothic.ttf', size = 27))
   idraw.text((570, 410), str(len(gg.members)), (255, 255, 255), font = ImageFont.truetype(r'./Gothic.ttf', size = 27))
   idraw.text((503, 470), str(len(await gg.bans())), (255, 255, 255), font = ImageFont.truetype(r'./Gothic.ttf', size = 27))
